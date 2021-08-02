3cafbcb4cc64bd27e7b80537ddf07c917e3228e5
Sia Qihan Ryan <67356207+RetMenTos@users.noreply.github.com>
2021-08-02 20:10:31 +0800

    MongoDB test

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const router = require("./routes")
+const bodyParser = require("body-parser")
+
 const app = express();
 
 const PORT = process.env.PORT || 8000
+
+mongoose.connect( `mongodb+srv://Admin:Admin123@cluster0.nqclt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(router);
 
 app.listen(PORT, () =>{
     console.log("Listening on port " + PORT);
 })
 
-app.get('/', (req, res) => {
-    res.send({ ok: true });
+app.get('/testing', (req, res) => {
+    res.send({ yeah: "it works" });
 })  
